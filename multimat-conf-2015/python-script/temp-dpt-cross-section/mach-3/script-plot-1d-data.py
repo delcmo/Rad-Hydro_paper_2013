44590385e03274b887361ac508b8a173ac460ef6
delcmo <mxd@ornl.gov>
2016-06-18 07:24:09 -0400

    update

--- multimat-conf-2015/python-script/temp-dpt-cross-section/mach-3/script-plot-1d-data.py
+++ multimat-conf-2015/python-script/temp-dpt-cross-section/mach-3/script-plot-1d-data.py
@@ -16,14 +16,14 @@ from decimal import *
 def plot_error_norms(nb_cells, L1_norm, L2_norm, variable):
   nb_cells_log = [math.log(x) for x in nb_cells]
   L1_norm_log = [math.log(x) for x in L1_norm]
-  L2_norm_log= [math.log(x) for x in L2_norm]
-  plt.plot(nb_cells_log, L1_norm_log, '+-', label=r'$L_1^{error} norm$', linewidth=3)
+#  L2_norm_log= [math.log(x) for x in L2_norm]
+  plt.plot(nb_cells_log, L1_norm_log, '+-', label=r'$L_1^{error} norm$')
   x1 = [math.log(nb_cells[0]), math.log(nb_cells[-1])]
-  y1 = [-2*math.log(nb_cells[0])+math.log(L1_norm[-1])+2*math.log(nb_cells[-1]), math.log(L1_norm[-1])]
-  plt.plot(x1, y1, '-', label=r'$line \ of  \ slope \ 2$', linewidth=3)
-  plt.plot(nb_cells_log, L2_norm_log, 'o-', label=r'$L_2^{error} norm$', linewidth=3)
-  y2 = [-2*math.log(nb_cells[0])+math.log(L2_norm[-1])+2*math.log(nb_cells[-1]), math.log(L2_norm[-1])]
-  plt.plot(x1, y2, '-', label=r'$line \ of  \ slope \ 2$', linewidth=3)
+  y1 = [-math.log(nb_cells[0])+math.log(L1_norm[-1])+math.log(nb_cells[-1]), math.log(L1_norm[-1])]
+  plt.plot(x1, y1, '-', label=r'$line \ of  \ slope \ 1$')
+#  plt.plot(nb_cells_log, L2_norm_log, 'o-', label=r'$L_2^{error} norm$')
+#  y2 = [-math.log(nb_cells[0])+math.log(L2_norm[-1])+math.log(nb_cells[-1]), math.log(L2_norm[-1])]
+#  plt.plot(x1, y2, '-', label=r'$line \ of  \ slope \ 2$')
   plt.legend(loc='best', fontsize=20, frameon=False)
   plt.xlabel(r'$\log (cells)$', fontsize=20)
   if variable=='density':
@@ -45,8 +45,8 @@ def plot_error_norms(nb_cells, L1_norm, L2_norm, variable):
 def plot_solution(x_num, y_num, x_anal, y_anal, x_offset, variable, nb_cells, text_on=False, mass_diff=0):
   x_anal_offset = [float(x)+float(x_offset) for x in x_anal]
   x_num = [float(x) for x in x_num]
-  plt.plot(x_num, y_num, '+-', markevery=8, markersize=10, label=r'$numerical \ solution$', linewidth=3)
-  plt.plot(x_anal_offset, y_anal, 'o-', markevery=300, markersize=10, label=r'$exact \ solution$', linewidth=2.2)
+  plt.plot(x_num, y_num, '+-', markevery=30, markersize=8, label=r'$numerical \ solution$', linewidth=2)
+  plt.plot(x_anal_offset, y_anal, 'o-', markevery=5000, markersize=8, label=r'$exact \ solution$', linewidth=1.2)
   plt.legend(loc='best', fontsize=20, frameon=False)
   plt.xlabel(r'$x$', fontsize=20)
   if variable=='density':
@@ -65,7 +65,7 @@ def plot_solution(x_num, y_num, x_anal, y_anal, x_offset, variable, nb_cells, te
   plt.xlim(float(min(x_num)), float(max(x_num)))
   plt.margins(0.1, 0.1)
   if text_on:
-    plt.figtext(0.2,0.5,r'$\Delta \rho$='+str('{:.4e}'.format(mass_diff)), fontsize=20)
+    plt.figtext(0.2,0.5,r'$\Delta \rho$='+str('{:.4e}'.format(mass_diff)), fontsize=14)
   fig_name=out_file+'-'+variable+'-nel-'+str(nb_cells)+'-plot.eps'
   plt.savefig(fig_name)
   print 'Saving plot using Matplotlib:', fig_name
@@ -82,12 +82,12 @@ def plot_visc_coeff(x_num, file, var_index, nb_cells):
     visc_e.append(row[var_index[5]])
     visc_max.append(row[var_index[6]])
   x_num = [float(x) for x in x_num[:-1]]
-  plt.semilogy(x_num, visc_max, '+-', markevery=10, markersize=10, label=r'$\kappa_{max}$', linewidth=3)
-  plt.semilogy(x_num, visc_e, 'o-', markevery=10, markersize=10, label=r'$\kappa_e$', linewidth=3)
+  plt.plot(x_num, visc_max, '+-', markevery=30, markersize=8, label=r'$\kappa_{max}$', linewidth=2)
+  plt.plot(x_num, visc_e, 'o-', markevery=30, markersize=8, label=r'$\kappa_e$', linewidth=2)
   plt.legend(loc='best', fontsize=20, frameon=False)
   plt.xlabel(r'$x$', fontsize=20)
   plt.ylabel(r'$\kappa$', fontsize=20)
-#  plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+  plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
   plt.xlim(float(min(x_num)), float(max(x_num)))
   plt.margins(0.1, 0.1)
   fig_name=out_file+'-visc-nel-'+str(nb_cells)+'-plot.eps'
@@ -100,27 +100,27 @@ def plot_visc_coeff(x_num, file, var_index, nb_cells):
 file_exact_list = []
 path_to_exact_files = 'test'
 # x-coordinates
-file_exact_list.append('x_1p05.txt')
+file_exact_list.append('data_x.dat')
 x_coord_exact = []
 file_data_exact=open(file_exact_list[-1], 'r')
 x_coord_exact[:] = [ line[:-1] for line in file_data_exact]
 # material density
-file_exact_list.append('rho_1p05.txt')
+file_exact_list.append('data_density.dat')
 mat_density_exact = []
 file_data_exact=open(file_exact_list[-1], 'r')
 mat_density_exact[:] = [ line[:-1] for line in file_data_exact]
 # radiation energy density
-file_exact_list.append('Er_1p05.txt')
+file_exact_list.append('data_RED.dat')
 radiation_exact = []
 file_data_exact=open(file_exact_list[-1], 'r')
 radiation_exact[:] = [ line[:-1] for line in file_data_exact]
 # mach number or fluid velocity
-file_exact_list.append('mach_1p05.txt')
+file_exact_list.append('data_Mach.dat')
 mach_nb_exact = []
 file_data_exact=open(file_exact_list[-1], 'r')
 mach_nb_exact[:] = [ line[:-1] for line in file_data_exact]
 # material temperature
-file_exact_list.append('T_1p05.txt')
+file_exact_list.append('data_Temp.dat')
 mat_temp_exact = []
 file_data_exact=open(file_exact_list[-1], 'r')
 mat_temp_exact[:] = [ line[:-1] for line in file_data_exact]
@@ -139,38 +139,62 @@ mach_nb_exact = [ float(i)/float(mach_nb_exact[0]) for i in mach_nb_exact]
 
 # SET INPUT FILES
 file_list = []
-file_list.append('mach-1p05-nel-10-points0.csv')
-#file_list.append('mach-1p05-nel-20-points0.csv')
-#file_list.append('mach-1p05-nel-30-points0.csv')
-file_list.append('mach-1p05-nel-40-points0.csv')
-file_list.append('mach-1p05-nel-50-points0.csv')
-##file_list.append('mach-1p05-nel-60-points0.csv')
-file_list.append('mach-1p05-nel-70-points0.csv')
-#file_list.append('mach-1p05-nel-80-points0.csv')
-#file_list.append('mach-1p05-nel-90-points0.csv')
-file_list.append('mach-1p05-nel-100-points0.csv')
-#file_list.append('mach-1p05-nel-110-points0.csv')
-#file_list.append('mach-1p05-nel-120-points0.csv')
-file_list.append('mach-1p05-nel-130-points0.csv')
-#file_list.append('mach-1p05-nel-140-points0.csv')
-#file_list.append('mach-1p05-nel-150-points0.csv')
-file_list.append('mach-1p05-nel-160-points0.csv')
-#file_list.append('mach-1p05-nel-170-points0.csv')
-#file_list.append('mach-1p05-nel-180-points0.csv')
-file_list.append('mach-1p05-nel-190-points0.csv')
-file_list.append('mach-1p05-nel-200-points0.csv')
-file_list.append('mach-1p05-nel-210-points0.csv')
-file_list.append('mach-1p05-nel-220-points0.csv')
-file_list.append('mach-1p05-nel-230-points0.csv')
-file_list.append('mach-1p05-nel-240-points0.csv')
-file_list.append('mach-1p05-nel-250-points0.csv')
+
+#file_list.append('mach-3-nel-250-points0.csv')
+#file_list.append('mach-3-nel-300-points0.csv')
+
+# above 1e-10 mass difference
+#file_list.append('mach-3-nel-350-points0.csv')
+#file_list.append('mach-3-nel-400-points0.csv')
+#file_list.append('mach-3-nel-500-points0.csv')
+##file_list.append('mach-3-nel-600-points0.csv')
+#file_list.append('mach-3-nel-650-points0.csv')
+#file_list.append('mach-3-nel-700-points0.csv')
+#file_list.append('mach-3-nel-850-points0.csv')
+#file_list.append('mach-3-nel-900-points0.csv')
+##file_list.append('mach-3-nel-1000-points0.csv')
+##file_list.append('mach-3-nel-1500-points0.csv')
+#file_list.append('mach-3-nel-2200-points0.csv')
+
+# below 1e-10 mass difference
+#file_list.append('mach-3-nel-450-points0.csv')
+#file_list.append('mach-3-nel-550-points0.csv')
+#file_list.append('mach-3-nel-750-points0.csv')
+#file_list.append('mach-3-nel-800-points0.csv')
+#file_list.append('mach-3-nel-950-points0.csv')
+#file_list.append('mach-3-nel-1200-points0.csv')
+#file_list.append('mach-3-nel-2000-points0.csv')
+#file_list.append('mach-3-nel-2500-points0.csv')
+
+#file_list.append('mach-3-nel-300-points0.csv')
+#file_list.append('mach-3-nel-400-points0.csv')
+#file_list.append('mach-3-nel-500-points0.csv')
+##file_list.append('mach-3-nel-600-points0.csv')
+#file_list.append('mach-3-nel-700-points0.csv')
+#file_list.append('mach-3-nel-800-points0.csv')
+#file_list.append('mach-3-nel-900-points0.csv')
+#file_list.append('mach-3-nel-1000-points0.csv')
+#file_list.append('mach-3-nel-2000-points0.csv')
+
+## works well with cfl=0.1
+#file_list.append('mach-3-nel-100-points0.csv')
+file_list.append('mach-3-nel-200-points0.csv')
+file_list.append('mach-3-nel-400-points0.csv')
+file_list.append('mach-3-nel-500-points0.csv')
+file_list.append('mach-3-nel-600-points0.csv')
+file_list.append('mach-3-nel-700-points0.csv')
+file_list.append('mach-3-nel-800-points0.csv')
+file_list.append('mach-3-nel-900-points0.csv')
+file_list.append('mach-3-nel-1000-points0.csv')
+#file_list.append('mach-3-nel-1100-points0.csv')
+#file_list.append('mach-3-nel-1600-points0.csv')
 
 # SET SOME VARIABLES
 dir_path = os.getcwd()
+quad_order = 100 # 70
 interp_kind = 'linear'
-quad_order = 20
 nb_files = len(file_list)
-var_index = [11, 5, 1, 2, 8, 4, 3] # [x, rho, radiation, mach, mat temp, visc_e, visc_mac]
+var_index = [11, 5, 1, 2, 8, 4, 3] # [x, rho, radiation, mach, mat temp]
 var_index[:] = [i -1 for i in var_index] # convert to python index
 
 # OUTPUT SOME INFORMATION
@@ -203,7 +227,7 @@ for file in file_list:
   print '------------------------------'
   print 'Name of the input file:', file
   # set/reset data
-  out_file = file[:9]
+  out_file = file[:6]
   mat_temp[:] = []
   mach_nb[:] = []
   radiation[:] = []
@@ -230,11 +254,13 @@ for file in file_list:
   nb_cells.append(len(x_coord)-1)
   print'Number of cells in file', file, ':', nb_cells[-1]
 
-#  mass_diff = compute_mass_diff(0., x_coord, mat_density, x_coord_exact, mat_density_exact, quad_order,)
+#  mass_diff = compute_mass_diff(0., x_coord, mat_density, x_coord_exact, mat_density_exact, quad_order, interp_kind)
+#  print mass_diff
+#  sys.exit()
 #  res = [0,mass_diff]
   # minimize the mass difference between the exact and numerical solutions to get 'x_offset'
 ##  res = fmin(compute_mass_diff, 0., args=(x_coord, mat_density, x_coord_exact, mat_density_exact, quad_order, interp_kind,), xtol=1., ftol=1., full_output=True, disp=True, retall=True)[0:2]
-  res = fmin(compute_mass_diff, 2.e-4, args=(x_coord, mat_density, x_coord_exact, mat_density_exact, quad_order, interp_kind,), xtol=1.e-15, ftol=1.e-20, full_output=True, disp=True, retall=True)[0:2]
+  res = fmin(compute_mass_diff, 0., args=(x_coord, mat_density, x_coord_exact, mat_density_exact, quad_order, interp_kind,), xtol=1.e-20, ftol=1e-10, full_output=True, disp=True, retall=True)[0:2]
 ##  res = minimize(compute_mass_diff, 2.e-4, args=(x_coord, mat_density, x_coord_exact, mat_density_exact, quad_order, interp_kind,), method='nelder-mead', options={'xtol': 1e-4, 'disp': True, 'maxiter' : 10000})
   x_offset.append(float(res[0]))
   mass_diff = res[1]
@@ -244,18 +270,22 @@ for file in file_list:
   plot_solution(x_coord, mat_density, x_coord_exact, mat_density_exact, x_offset[-1], 'density', nb_cells[-1], True, mass_diff)
   # compute the error norms for density
   l1_norm, l2_norm = compute_error_norms(x_offset[-1], x_coord, mat_density, x_coord_exact, mat_density_exact, quad_order, interp_kind)
+  print l1_norm
   L1_norm_density.append(l1_norm)
   L2_norm_density.append(l2_norm)
   # compute the error norms for radiation
   l1_norm, l2_norm = compute_error_norms(x_offset[-1], x_coord, radiation, x_coord_exact, radiation_exact, quad_order, interp_kind)
+  print l1_norm
   L1_norm_radiation.append(l1_norm)
   L2_norm_radiation.append(l2_norm)
   # compute the error norms for mach number
   l1_norm, l2_norm = compute_error_norms(x_offset[-1], x_coord, mach_nb, x_coord_exact, mach_nb_exact, quad_order, interp_kind)
+  print l1_norm
   L1_norm_mach.append(l1_norm)
   L2_norm_mach.append(l2_norm)
   # compute the error norms for material temperature
   l1_norm, l2_norm = compute_error_norms(x_offset[-1], x_coord, mat_temp, x_coord_exact, mat_temp_exact, quad_order, interp_kind)
+  print l1_norm
   L1_norm_mat_temp.append(l1_norm)
   L2_norm_mat_temp.append(l2_norm)
 
@@ -273,11 +303,12 @@ plot_error_norms(nb_cells, L1_norm_mat_temp, L2_norm_mat_temp, 'mat-temp')
 plot_solution(x_coord, radiation, x_coord_exact, radiation_exact, x_offset[-1], 'radiation', nb_cells[-1])
 plot_solution(x_coord, mach_nb, x_coord_exact, mach_nb_exact, x_offset[-1], 'mach-number', nb_cells[-1])
 plot_solution(x_coord, mat_temp, x_coord_exact, mat_temp_exact, x_offset[-1], 'mat-temp', nb_cells[-1])
-plot_visc_coeff(x_coord, 'test0.csv', var_index, nb_cells[-1])
+plot_visc_coeff(x_coord, 'visc-coeff-nel-1000-points0.csv', var_index, nb_cells[-1])
+#plot_visc_coeff(x_coord, 'visc-coeff-nel-1600-points0.csv', var_index, nb_cells[-1])
 
 # PLOT X_OFFSET AND SAVE VALUES IN FILE
 # plot
-plt.plot(nb_cells, x_offset, '-o', markersize=10, linewidth=3)
+plt.plot(nb_cells, x_offset, '-o', markersize=8, linewidth=2)
 plt.xlabel(r'$cells$', fontsize=20)
 plt.ylabel(r'$x_{offset}$', fontsize=20)
 plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
